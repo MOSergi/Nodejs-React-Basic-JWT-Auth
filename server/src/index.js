e3914a5ba3cb0bfd2d5c40c9b351af6e08eeb24c
MOSergi <sergioorozmendez@gmail.com>
2022-03-05 14:57:55 +0100

    comenzando con el backend

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+//requires 
+const express = require("express");
+
+//iniciar express
+const app = express();
+
+//settings
+app.set("PORT", process.env.PORT || 4000);
+
+
+//basic middleware
+
+
+//routes
+
+
+
+//server listen
