73fb77c4fb865cb5f9fc8693bf41080197d858ef
MOSergi <sergioorozmendez@gmail.com>
2022-03-06 11:04:39 +0100

    Arreglado error con la conexión a la bd

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,14 +1,26 @@
 const { application, json } = require("express");
 const express = require("express");
 const mysql = require("mysql");
+const dbDt = require("../database/conection.js");
 
 const rutas = express.Router();
+
+
+//configurando conexion a la base de datos
+const conexion = mysql.createConnection(dbDt);
+conexion.connect();
+
 
 rutas.post("/Registrar", (req, res)=>{
 
     let userData = req.body;
 
-    res.json(userData);
+    conexion.query("SELECT * FROM USUARIOS", (err, rows, fileds)=>{
+        console.log(rows);
+        console.log(fileds);
+    })
+
+    res.json("works");
 
 });
 
