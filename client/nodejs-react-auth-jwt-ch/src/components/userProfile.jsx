3a2f3ed7f0ae4f5c7167cafb28c08216294799d5
MOSergi <sergioorozmendez@gmail.com>
2022-03-08 16:30:42 +0100

    añado el componente de perfil de usuario

---
+++ client/nodejs-react-auth-jwt-ch/src/components/userProfile.jsx
@@ -0,0 +1,56 @@
+import { React, useContext, useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import UserContext from "../context/userContext";
+import "../styles/Navbar.css";
+
+function Profile(){
+
+    const {loginStatus, setLoginStatus} = useContext(UserContext);
+    const [username, setUsername] = useState("");
+
+    const navegar = useNavigate();
+
+    useEffect(()=>{
+        fetch("http://localhost:4000/Profile",{
+            credentials : "include"
+        })
+        .then(respuesta => respuesta.json())
+        .then((info) =>{
+            if (info == "Invalid Token"){
+                setLoginStatus(false);
+                navegar("/Login/");
+            } else if (info == "NoToken") {
+                setLoginStatus(false);
+                navegar("/Login/");
+            } else {
+                setUsername(info);
+                setLoginStatus(true);
+            } 
+        })
+        .catch(error => console.log(error))
+    }, [loginStatus])
+
+    const logout = ()=>{
+        fetch("http://localhost:4000/Logout",{
+            credentials : "include",
+        })
+        .then(respuestaa => respuestaa.json())
+        .then((values) =>{
+            if (values == "Success Logout"){
+                setLoginStatus(false);
+                navegar("/Login/");
+            }
+        })
+        .catch(error => console.log(error))
+    }
+
+    return(
+        <section style={{"textAlign": "center"}}>
+            <h2>Bienvenido {username} </h2>
+            <button onClick={()=>{logout()}} style={{"padding" : "10px", "marginTop" : "10px", "cursor": "pointer"}}>Logout</button>
+        </section>
+    );
+}
+
+
+export default Profile;
