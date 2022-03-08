9f0d23b92067e89877f8e877c5b467bae2556358
MOSergi <sergioorozmendez@gmail.com>
2022-03-08 16:33:17 +0100

    llamo a los nuevos componentes

--- client/nodejs-react-auth-jwt-ch/src/App.jsx
+++ client/nodejs-react-auth-jwt-ch/src/App.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/App.css';
 import Navbar from "./components/navbar";
@@ -7,6 +7,7 @@ import Footer from "./components/footer";
 import Register from "./components/register";
 import Login from "./components/login";
 import UserContext from "./context/userContext";
+import Profile from "./components/userProfile";
 
 function App() {
 
@@ -18,12 +19,13 @@ function App() {
       <Router>
         <UserContext.Provider value={{loginStatus, setLoginStatus, login, setLogin}}>
           <Routes>
-                <Route path="/" element={<><Navbar/> <Section/> <Footer/></>}></Route>
+                <Route path="/" element={<><Navbar/> <Section/> <Footer/> </>}></Route>
                 <Route path="/Register" element={<><Navbar/> <Register/></>}></Route>
                 <Route path="/Login" element={<><Navbar/> <Login/></>}></Route>
+                <Route path="/Profile" element={<><Navbar/> <Profile /></>}></Route>
           </Routes>
         </UserContext.Provider>  
-      </Router> 
+      </Router>
     </div>
   );
 }
