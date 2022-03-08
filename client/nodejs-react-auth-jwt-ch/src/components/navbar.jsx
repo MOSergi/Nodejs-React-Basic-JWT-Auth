be55f044821c0a4ad98edcca5c3cb2349cc6a1c0
MOSergi <sergioorozmendez@gmail.com>
2022-03-08 16:32:08 +0100

    renderizado condicional menu a partir loginStatus

--- client/nodejs-react-auth-jwt-ch/src/components/navbar.jsx
+++ client/nodejs-react-auth-jwt-ch/src/components/navbar.jsx
@@ -1,15 +1,47 @@
-import { React } from "react";
+import { React, useContext, useEffect} from "react";
 import {Link} from "react-router-dom";
+import UserContext from "../context/userContext";
 import "../styles/Navbar.css";
 
 function Navbar(){
-    return(
-        <nav className="menu">
-            <Link className="links" to="/">Home</Link>
-            <Link className="links link-g" to="/Register">Registro</Link>
-            <Link className="links" to="/Login">Login</Link>
-        </nav>
-    );
+
+
+    const {loginStatus, setLoginStatus} = useContext(UserContext);
+
+    useEffect(()=>{
+        fetch("http://localhost:4000/validateLoged", {
+            credentials : "include"
+        })
+        .then(respuesta => respuesta.json())
+        .then((datos) =>{
+            if (datos == "NoToken"){
+                setLoginStatus(false);
+            } else if (datos == "Invalid Token"){
+                setLoginStatus(false);
+            } else if (datos == "LogedIn"){
+                setLoginStatus(true);
+            }
+        })
+        .catch(error => console.log(error))
+
+    }, [loginStatus])
+
+    if (loginStatus == true){
+        return(
+            <nav className="menu">
+                <Link className="links" to="/">Home</Link>
+                <Link className="links" to="/Profile">Perfil</Link>
+            </nav>
+        );
+    } else {
+        return(
+            <nav className="menu">
+                <Link className="links" to="/">Home</Link>
+                <Link className="links link-g" to="/Register">Registro</Link>
+                <Link className="links" to="/Login">Login</Link>
+            </nav>
+        );
+    }
 }
 
 
